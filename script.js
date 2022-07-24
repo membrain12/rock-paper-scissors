f5c78cf78f37a0c913976b8afd9cedec0553579a
Paul Campbell <apcampbell12@gmail.com>
2022-07-24 15:42:11 -0500

    Add function to play round of game

--- script.js
+++ script.js
@@ -8,4 +8,28 @@ function getComputerChoice() {
     return keys[option];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+    let comp = computerSelection.toLowerCase();
+
+    let result = true;
+
+    if (player === comp) {
+        return "Tie!";
+    } else if (player == "rock" && comp == "paper") {
+        result = false;
+    } else if (player == "scissors" && comp == "rock") {
+        result = false;
+    } else if (player == "paper" && comp == "scissors") {
+        result = false;
+    } else {
+        result = true;
+    }
+
+    if (result) {
+        return `You Win! ${player} beats ${comp}`;
+    } else {
+        return `You Lose! ${comp} beats ${player}`;
+    }
+}
+
